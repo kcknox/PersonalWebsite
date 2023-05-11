2a3e435e9d25e01ba86916d0aab39c0b5d9351f5
Kenneth Knox <kcknox51@utexas.edu>
2023-05-11 12:58:25 -0500

    Finished Contact Page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,13 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from "../AnimatedLetters";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
 
     useEffect(() => {
         let timeoutId = setTimeout(() => {
@@ -14,6 +17,27 @@ const Contact = () => {
                     clearTimeout(timeoutId)
                 }
     }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'default_service',
+                'template_9p0mtab',
+                form.current,
+                '45E-kFGveuYt25zS3'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again.')
+                }
+            )
+    }
 
     return (
         <>
@@ -28,12 +52,12 @@ const Contact = () => {
                     </h1>
                     <p>
                         I am currently seeking employment from an IT company for 
-                        the summer and post graduation. If you have a question,
+                        the summer and post graduation (May 2024). If you have a question,
                         feedback, or just want to say hello, I'd love to hear from
                         you! Use the form below to send me a message.
                     </p>
                     <div className='contact-form'>
-                        <form>
+                        <form ref={form} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input type="text" name="name" placeholder="Name" required />
@@ -53,6 +77,23 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Kenneth Colby Knox
+                    <br />
+                    Austin, Texas,
+                    <br />
+                    United States 
+                    <br />
+                    <span> cknox2468@gmail.com </span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[30.289297, -97.744608]} zoom={14}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[30.289297, -97.744608]}>
+                            <Popup> You can find me around here! </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman' />
