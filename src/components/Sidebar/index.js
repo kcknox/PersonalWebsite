01a904f0886bfb374780393a9c26d2de86e2fe52
Kenneth Knox <kcknox51@utexas.edu>
2023-09-12 12:31:13 -0500

    Homepage mobilized

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -26,23 +26,23 @@ const Sidebar = () => {
             <img className="sub-logo" src={LogoSubtitle} alt="logosub" />
         </Link>
         <nav className={showNav ? 'mobile-show' : ''}> 
-            <NavLink exact="true" activeclassname="active" to="/" >
+            <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" >
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="experience-link" to="/experience" >
+            <NavLink exact="true" activeclassname="active" className="experience-link" to="/experience" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
             </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects" >
+            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faHammer} color="#4d4d4e" />
             </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" >
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
             <FontAwesomeIcon 
